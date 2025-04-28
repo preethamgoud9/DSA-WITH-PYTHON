e80e914012fbb18a63b457f3a31e8779818c8c95
preethamgoud9 <preethamgoud229@gmail.com>
2025-04-29 01:57:38 +0530

    ctypes

--- static-array.py
+++ static-array.py
@@ -1,8 +1,8 @@
 class MyArray:
-    def __init__(self,capacity=10):
+    def __init__(self,capacity=1):
         "Initializing the Array with fixed capacity of 10 here"
-        self.capacity = capacity
-        self.size = 0
+        self.capacity = capacity   #self.size
+        self.size = 0   #self.n
         self.array = [None] * self.capacity
 
     def append(self,element):
@@ -23,18 +23,44 @@ class MyArray:
 
     def __getitem__(self,index):
         "Access an element by element"
-        if index > self.size or index <= 0:
+        if index >= self.size or index < 0:
             raise IndexError("Index is out of range")
         return self.array[index]
     
     def __setitem__(self,index,value):
         "set an element at the specified value"
-        if index > self.size or index <= 0:
+        if index >= self.size or index < 0:
             raise IndexError("Index is out of range")
         self.array[index] = value
     
-    def len(self):
+    def __len__(self):
         "Return the current size of the array(Number of elements)"
         return self.size
 
     def str(self):
+        pass
+
+    def print(self):
+        pass
+
+    def pop():
+        pass
+
+    def clear():
+        pass
+
+    def find():
+        pass
+
+    def insert():
+        pass
+    
+    def delete():
+        pass
+
+    def remove():
+        pass
+
+
+L = MyArray()
+print(type(L))
